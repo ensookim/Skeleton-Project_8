cd21197ee2dcaa393c2781e4e0b2e1f3ca665a86
Gyeongjo <119joe@naver.com>
2025-04-07 17:52:21 +0900

    프로젝트 추가

---
+++ src/router/index.js
@@ -0,0 +1,57 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '@/views/HomePage.vue';
+import AddPage from '@/views/AddPage.vue';
+import EditPage from '@/views/EditPage.vue';
+import ProfilePage from '@/views/ProfilePage.vue';
+import ProfileEdit from '@/views/ProfileEdit.vue';
+import TransactionPage from '@/views/TransactionPage.vue';
+import TransactionDetailPage from '@/views/TransactionDetailPage.vue';
+import NotFound from '@/views/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'HomePage',
+      component: HomePage,
+    },
+    {
+      path: '/transaction',
+      name: 'TransactionPage',
+      component: TransactionPage,
+    },
+    {
+      path: '/transaction/:id',
+      name: 'TransactionDetailPage',
+      component: TransactionDetailPage,
+    },
+    {
+      path: '/transaction/add',
+      name: 'AddPage',
+      component: AddPage,
+    },
+    {
+      path: '/transaction/edit/:id',
+      name: 'EditPage',
+      component: EditPage,
+    },
+    {
+      path: '/user/:id',
+      name: 'ProfilePage',
+      component: ProfilePage,
+    },
+    {
+      path: '/user/edit/:id',
+      name: 'ProfileEdit',
+      component: ProfileEdit,
+    },
+    {
+      path: '/:paths(.*)*',
+      name: 'NotFound',
+      component: NotFound,
+    },
+  ],
+});
+
+export default router;
