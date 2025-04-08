b28c0df336c1b50242e25791020fc8a0606badd6
Gyeongjo <119joe@naver.com>
2025-04-08 17:44:01 +0900

    최근 거래 내역 5개 출력

--- src/components/RecentHistory.vue
+++ src/components/RecentHistory.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <h1>최근 거래 내역</h1>
+    <ul>
+      <TransactionItem
+        v-for="(trans, index) in recentTransactions"
+        :key="trans.id"
+        :trans="trans"
+        :index="index"
+      />
+    </ul>
     <button @click="goToAdd">추가하기</button>
     <button @click="goToTransaction">더보기</button>
   </div>
@@ -9,10 +17,21 @@
 <script setup>
 import { useRouter } from 'vue-router';
 import { useCategoryStore } from '@/stores/categoryStore';
+import { useTransactionStore } from '@/stores/transactionStore';
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
+import TransactionItem from './TransactionItem.vue';
 
-const store = useCategoryStore();
+const store_category = useCategoryStore();
+const store_trans = useTransactionStore();
 
 const router = useRouter();
+
+store_trans.fetchTransactions();
+
+const { transactions } = storeToRefs(store_trans);
+const recentTransactions = computed(() => transactions.value.slice(0, 5));
+
 const goToAdd = () => {
   router.push('/transaction/add');
 };
