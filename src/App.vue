51f4f703d186ebb319a880b55feb7109837a2c85
Gyeongjo <119joe@naver.com>
2025-04-08 14:48:27 +0900

    최근 내역 컴포넌트 파일 추가

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
+    <Header />
     <MonthlyReport />
     <TransactionPage />
+    <Footer />
   </div>
 </template>
 
 <script setup>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 import MonthlyReport from './components/MonthlyReport.vue';
 import TransactionPage from './views/TransactionPage.vue';
 </script>
