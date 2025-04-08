6210cf318ad994b442e06f1e5f5cb2b9e5f65ba6
Gyeongjo <119joe@naver.com>
2025-04-08 17:35:09 +0900

    최근 거래 내역 추가하기, 더보기 추가

--- src/components/RecentHistory.vue
+++ src/components/RecentHistory.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
     <h1>최근 거래 내역</h1>
-    <router-link :to="{ name: 'TransactionPage' }" class="mx-1"
-      ><button>더보기</button></router-link
-    >
+    <button @click="goToAdd">추가하기</button>
+    <button @click="goToTransaction">더보기</button>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+import { useCategoryStore } from '@/stores/categoryStore';
+
+const store = useCategoryStore();
+
+const router = useRouter();
+const goToAdd = () => {
+  router.push('/transaction/add');
+};
+const goToTransaction = () => {
+  router.push('/transaction');
+};
+</script>
 
 <style scoped></style>
