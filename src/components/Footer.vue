0265f8a3ca7cdab626549d0109609f6cb9ae8617
JunSik Yun <98214965+JunSikYun@users.noreply.github.com>
2025-04-09 17:18:34 +0900

    align-left & Footer backgroundColor change

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,17 @@
     <p>© 2025 가계부 프로젝트</p>
   </footer>
 </template>
+
 <script setup></script>
+
 <style scoped>
 footer {
-  background-color: #f9fbfd;
+  background-color: grey;
   padding: 1rem;
   text-align: center;
   width: 100%;
+  color: white;
+  font-weight: 500;
   box-shadow: 0 -2px 6px rgba(0, 0, 0, 0.05);
   bottom: 0;
 }
