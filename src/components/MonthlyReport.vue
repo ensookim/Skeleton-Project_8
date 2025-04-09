a561f5cc549643abd5214a850f642eae3f2dfccf
Wonyoung Kim <wonyoung020626@gmail.com>
2025-04-09 21:51:51 +0900

    refactor : Add comparison of Net Profit

--- src/components/MonthlyReport.vue
+++ src/components/MonthlyReport.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card rounded-4 d-flex flex-row p-3 m-5">
     <!-- 이번 달 총 지출 -->
-    <div class="card-body d-flex flex-row rounded mx-5">
+    <div class="card-body d-flex flex-row rounded mx-2">
       <div>
         <i class="fa-solid fa-minus bg-danger rounded-circle p-1"></i>
       </div>
@@ -60,6 +60,35 @@
         <h4 class="card-title">이번 달 순수익</h4>
         <p class="card-text">
           : {{ formatCurrency(currentIncome - currentExpense) }}
+        </p>
+        <p>
+          <span class="text-muted me-2">전월 대비</span>
+          <span
+            v-if="
+              currentIncome - lastIncome - (currentExpense - lastExpense) > 0
+            "
+            style="color: green"
+          >
+            <i class="fa-solid fa-arrow-up"> </i>
+            {{
+              formatCurrency(
+                currentIncome - lastIncome - (currentExpense - lastExpense)
+              )
+            }}
+          </span>
+          <span
+            v-if="
+              currentIncome - lastIncome - (currentExpense - lastExpense) < 0
+            "
+            style="color: red"
+          >
+            <i class="fa-solid fa-arrow-down"> </i>
+            {{
+              formatCurrency(
+                currentIncome - lastIncome - (currentExpense - lastExpense)
+              )
+            }}
+          </span>
         </p>
       </div>
     </div>
